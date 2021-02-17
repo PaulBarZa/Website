ab7a03662293d9ab22bea5d4101107083c31f147
PaulBarZa <paul.barthe@epfedu.fr>
2021-02-17 11:48:32 +0100

    feat: reveal element when scroll

--- front/page.js
+++ front/page.js
@@ -1,18 +1,55 @@
-var actilityTitle = "Web application Frontend & Backend"
-var actilityProject = "From July to the end of December 2020, I did an internship in the company <a href='https://www.actility.com' target='_blank' class='link-actility'>Actility</a>. My project was to develop a web application to drive a LoRaWAN network.<br><br><span class='colored'>Key learnings:</span> Angular, Go, HTML/CSS, Typescript, REST API, LoRaWAN network operation, Linux."
+const actilityTitle = "Web application Frontend & Backend"
+const actilityProject = "From July to the end of December 2020, I did an internship in the company <a href='https://www.actility.com' target='_blank' class='link-actility'>Actility</a>. My project was to develop a web application to drive a LoRaWAN network.<br><br><span class='colored'>Key learnings:</span> Angular, Go, REST API, LoRaWAN network, Linux."
 
-var androidTitle = "Android Title"
-var androidProject ="This is the content of my future android project"
+const androidTitle = "Android Title"
+const androidProject ="This is the content of my future android project"
 
-var schoolTitle = "School Project Title"
-var schoolProject ="This is the content of my future school project"
+const schoolTitle = "School Project Title"
+const schoolProject ="This is the content of my future school project"
 
-var webSiteTitle = "This web site"
-var webSiteProject ="The objective of this project was to have fun while reinforcing my mastery of the basic tools of web development.<br><br><span class='colored'>Stack:</span> HTML/CSS, Javascript, Node.js."
+const webSiteTitle = "This web site"
+const webSiteProject ="The objective of this project was to have fun while reinforcing my mastery of the basic tools of web development.<br><br><span class='colored'>Stack:</span> HTML/CSS, Javascript, Node.js."
+
+const ratio = .3
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add("reveal-element")
+
+            var section_2 = document.getElementById("sec-2")
+            if(section_2.querySelector(".reveal-element") != null){
+
+                var i = 1
+                section_2.querySelectorAll('.circle').forEach(function(el){
+                    el.classList.add("circle-"+i)
+                    i++
+                })
+
+                var y = 1
+                section_2.querySelectorAll('.line').forEach(function(el){
+                    el.classList.add("line-"+y)
+                    y++
+                })
+            }
+            observer.unobserve(entry.target)
+        }
+    })
+}
 
 window.onload = function () {
     changeText(1);
     var form = document.getElementById("form").onsubmit = submitted.bind(form);
+
+    var observer = new IntersectionObserver(handleIntersect, options)
+    document.querySelectorAll('.reveal').forEach(function (r) {
+    observer.observe(r)
+})
 };
 
 function changeText(id){
@@ -32,7 +69,7 @@ function changeText(id){
         document.getElementById('text').innerHTML =schoolProject;
     } else if(id==4){
         document.getElementById('title').innerHTML = webSiteTitle;
-        document.getElementById('text').innerHTML =webSiteProject;
+        document.getElementById('text').innerHTML = webSiteProject;
     }
     document.getElementById('el'+id).style.color = "#ffce00";
 }
